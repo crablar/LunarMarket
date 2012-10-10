38ff42efd750885d97654e4c3b18e035b8c0971b
Joshua Stewart <joshstewart@utexas.edu>
2012-10-10 11:14:59 -0500

    add gameview skeleton

---
+++ src/com/jeffmeyerson/moonstocks/GameView.java
@@ -0,0 +1,24 @@
+package com.jeffmeyerson.moonstocks;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+class GameView extends View {
+	public GameView(Context context) {
+		super(context);
+	}
+
+	public GameView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public GameView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	public void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+	}
+}
