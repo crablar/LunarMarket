42dfbcb7b93b781e7526b180faafe5bad18420d0
Jeff Meyerson <meff.jeyerson@gmail.com>
2012-11-10 18:55:41 -0600

    I don't even understand EvilStockFunction anymore

--- src/com/jeffmeyerson/moonstocks/pricefunctions/EVILStockFunction.java
+++ src/com/jeffmeyerson/moonstocks/pricefunctions/EVILStockFunction.java
@@ -3,6 +3,8 @@ package com.jeffmeyerson.moonstocks.pricefunctions;
 import java.util.List;
 import java.util.Random;
 
+import android.util.Log;
+
 /**
  * A simple uptrend with an upper bound.
  * 
@@ -16,7 +18,7 @@ public class EVILStockFunction implements PriceFunction {
 	private static double amplifier = .1;
 	private static int fib1 = 0;
 	private static int fib2 = 0;
-	
+	private static int previousReturn = -1;
 
 	public static int getFib() {
 		if (fib1 == 0)
@@ -35,31 +37,51 @@ public class EVILStockFunction implements PriceFunction {
 
 	@Override
 	public int getValue(int time, List<Integer> values) {
-				
+
 		// for now, wrap around if time goes past the amount of song data we
 		// have
 		time = Math.abs(time) % values.size();
-		
+
 		// Variables to calculate the percentage change between musical points
-		int t1;
-		int t2;
-		
+		int vt1;
+		int vt2;
+
 		int result = 0;
 		if (time == 0) {
-			result = values.get(time) * 10;
+			vt1 = values.get(values.get(values.size() - 1));
+			vt2 = values.get(0);
+			result = vt2 * 10;
 		} else {
-			result = values.get(time) * values.get(time - 1) * 10;
+			vt1 = values.get(time - 1);
+			vt2 = values.get(time);
+
+			result = vt1 * vt2 * 10;
 		}
+
 		if (fluctuativeMultiplier % 3 == 0)
 			amplifier *= -1;
 		else
 			fluctuativeMultiplier += amplifier;
 
 		result += getFib() * fluctuativeMultiplier;
-		if (result < UPPER_BOUND && result > 0)
+		if (result > UPPER_BOUND || result < 0)
+			result = Math.abs(result % UPPER_BOUND);
+
+		double maxPercentChange = 1.0 + 1.0 * vt1 / vt2;
+
+		if (previousReturn == -1) {
+			previousReturn = result;
 			return result;
-		else
-			return Math.abs(result % UPPER_BOUND);
+		}
+
+		if (1.0 * result / previousReturn > maxPercentChange
+				|| 1.0 * previousReturn / result > maxPercentChange)
+			result = ((int) (maxPercentChange * previousReturn + 1) % UPPER_BOUND);
+		if(previousReturn == result)
+			result = getFib() * previousReturn;
+		previousReturn = result;
+		return result;
+
 	}
 
 }
